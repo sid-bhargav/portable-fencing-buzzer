4092943ff3498e0e213f4f21c46c54cba7e500d8
Siddharth Bhargav <sid.bhargav@gmail.com>
2023-05-09 17:59:27 -0700

    initial code

---
+++ fencing-buzzer/src/fencing-buzzer.ino
@@ -0,0 +1,20 @@
+// C++ code
+
+#define BUZZER_PIN 8
+#define BUTTON_PIN 7
+
+void setup()
+{
+    pinMode(BUZZER_PIN, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT);
+}
+
+void loop()
+{
+    if (digitalRead(BUTTON_PIN) == HIGH)
+    {
+        digitalWrite(BUZZER_PIN, HIGH);
+        delay(1000); // Wait for 1000 millisecond(s)
+        digitalWrite(BUZZER_PIN, LOW);
+    }
+}
