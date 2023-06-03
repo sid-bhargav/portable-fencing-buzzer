4db09a45b5aea48628e8401eb0782b79d779eeb4
Siddharth Bhargav <sid.bhargav@gmail.com>
2023-06-02 20:02:21 -0700

    updated circuit to use a pull-up resistor

--- fencing-buzzer/fencing-buzzer.ino
+++ fencing-buzzer/fencing-buzzer.ino
@@ -1,17 +1,19 @@
 // C++ code
 
-#define BUZZER_PIN 2
-#define BUTTON_PIN 7
+#define BUZZER_PIN 11
+#define BLADE_PIN 12
 
 void setup()
 {
     pinMode(BUZZER_PIN, OUTPUT);
-    pinMode(BUTTON_PIN, INPUT);
+    pinMode(BLADE_PIN, INPUT);
+
+    Serial.print("test");
 }
 
 void loop()
 {
-    if (digitalRead(BUTTON_PIN) == HIGH)
+    if (digitalRead(BLADE_PIN) == LOW)
     {
         digitalWrite(BUZZER_PIN, HIGH);
         delay(1000); // Wait for 1000 millisecond(s)
